dbcf91bffcddf5b8038f6326f91ae2ed77eb3c84
Your Name <you@example.com>
2016-01-15 15:31:07 +0800

    init files

---
+++ player_deBug.bat
@@ -0,0 +1,3 @@
+@echo off
+E:\Quick-Cocos2dx-Community\quick\player\win32\player3.exe -quick E:\Quick-Cocos2dx-Community\ -workdir E:\wjnezka2\ -file E:\wjnezka2\src\main.lua -writable E:\wjnezka2\ -size 960x640 -disable-write-debug-log -console -disable-load-framework -offset {50,20} -disable-debugger
+exit
